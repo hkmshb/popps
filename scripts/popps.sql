16dd88a7cb9f03298238b780fa57559638b666b3
Abdul-Hakeem Shaibu <hkmshb@gmail.com>
2018-08-01 11:57:40 +0100

    Modify fnGetWardPopSummary function signature

--- scripts/popps.sql
+++ scripts/popps.sql
@@ -68,26 +68,39 @@ BEGIN
   LANGUAGE 'plpgsql';
 
   -- FUNCTION: fnGetWardPopSummary
-  CREATE OR REPLACE FUNCTION fn_GetWardPopSummary (ageFrom INTEGER, ageTo INTEGER)
+  CREATE OR REPLACE FUNCTION fn_GetWardPopSummary (ageFrom INTEGER, ageTo INTEGER, sourceTarget VARCHAR)
   RETURNS TABLE (
     globalid VARCHAR,
     wardcode VARCHAR,
     popvalue FLOAT
   ) AS $FN2$
-  DECLARE
-    sourceTarget VARCHAR := 'Worldpop / ORNL NOT Adjusted';
   BEGIN
-    RETURN QUERY SELECT
-        vp.globalid,
-        vp.wardcode,
-        (SUM(CASE WHEN vp.gender='M' THEN value ELSE 0 END) +
-          SUM(CASE WHEN vp.gender='F' THEN value ELSE 0 END)
-        ) as "popvalue"
-    FROM vts_popestimate vp
-    WHERE vp.age_group_from = ageFROM
-    AND vp.age_group_to = ageTo
-    AND vp.source = sourceTarget
-    GROUP BY (vp.globalid, vp.featureidentifier, vp.wardcode);
+    IF ageFROM = 0 AND ageTO = 4 THEN
+      RAISE NOTICE 'GetWardPopSummary for 0-4';
+      RETURN QUERY SELECT
+          vp.globalid,
+          vp.wardcode,
+          (SUM(CASE WHEN vp.gender='M' THEN value ELSE 0 END) +
+            SUM(CASE WHEN vp.gender='F' THEN value ELSE 0 END)
+          ) as "popvalue"
+      FROM vts_popestimate vp
+      WHERE vp.age_group_to <= ageTo
+      AND vp.source = sourceTarget
+      GROUP BY (vp.globalid, vp.featureidentifier, vp.wardcode);
+    ELSE
+      RAISE NOTICE 'GetWardPopSummary for 5+';
+      RETURN QUERY SELECT
+          vp.globalid,
+          vp.wardcode,
+          (SUM(CASE WHEN vp.gender='M' THEN value ELSE 0 END) +
+            SUM(CASE WHEN vp.gender='F' THEN value ELSE 0 END)
+          ) as "popvalue"
+      FROM vts_popestimate vp
+      WHERE vp.age_group_from = ageFROM
+      AND vp.age_group_to = ageTo
+      AND vp.source = sourceTarget
+      GROUP BY (vp.globalid, vp.featureidentifier, vp.wardcode);
+    END IF;
   END $FN2$
   LANGUAGE 'plpgsql';
 
@@ -117,73 +130,78 @@ BEGIN
   LANGUAGE 'plpgsql';
 
   -- TABLE: settlement_pop
-  CREATE TABLE IF NOT EXISTS settlement_pop AS
-  SELECT
-      tbl1_4.globalid,
-      tbl1_4.wardcode,
-      tbl1_4.popvalue as "pop1_4",
-      tbl5_9.popvalue as "pop5_9",
-      tbl10_14.popvalue as "pop10_14",
-      tbl15_19.popvalue as "pop15_19",
-      tbl20_24.popvalue as "pop20_24",
-      tbl25_29.popvalue as "pop25_29",
-      tbl30_34.popvalue as "pop30_34",
-      tbl35_39.popvalue as "pop35_39",
-      tbl40_44.popvalue as "pop40_44",
-      tbl45_49.popvalue as "pop45_49",
-      tbl50_54.popvalue as "pop50_54",
-      tbl55_59.popvalue as "pop55_59",
-      tbl60_64.popvalue as "pop60_64",
-      tbl65_69.popvalue as "pop65_69",
-      tbl70_74.popvalue as "pop70_74",
-      tbl75_100.popvalue as "pop75_100",
-      (COALESCE(tbl1_4.popvalue, 0.0)   + COALESCE(tbl5_9.popvalue, 0.0) +
-      COALESCE(tbl10_14.popvalue, 0.0) + COALESCE(tbl15_19.popvalue, 0.0) +
-      COALESCE(tbl20_24.popvalue, 0.0) + COALESCE(tbl25_29.popvalue, 0.0) +
-      COALESCE(tbl30_34.popvalue, 0.0) + COALESCE(tbl35_39.popvalue, 0.0) +
-      COALESCE(tbl40_44.popvalue, 0.0) + COALESCE(tbl45_49.popvalue, 0.0) +
-      COALESCE(tbl50_54.popvalue, 0.0) + COALESCE(tbl55_59.popvalue, 0.0) +
-      COALESCE(tbl60_64.popvalue, 0.0) + COALESCE(tbl65_69.popvalue, 0.0) +
-      COALESCE(tbl70_74.popvalue, 0.0) + COALESCE(tbl75_100.popvalue, 0.0)
-      ) as "pop_total"
-  FROM fn_GetWardPopSummary(0, 4) as tbl1_4
-  LEFT JOIN fn_GetWardPopSummary(5, 9) as tbl5_9
-    ON tbl1_4.globalid = tbl5_9.globalid
+  CREATE OR REPLACE FUNCTION fn_CreateSettlementPopTable(sourceTarget VARCHAR)
+  RETURNS INTEGER AS $FF1$
+  BEGIN
+    CREATE TABLE IF NOT EXISTS settlement_pop AS
+    SELECT
+        tbl1_4.globalid,
+        tbl1_4.wardcode,
+        tbl1_4.popvalue as "pop1_4",
+        tbl5_9.popvalue as "pop5_9",
+        tbl10_14.popvalue as "pop10_14",
+        tbl15_19.popvalue as "pop15_19",
+        tbl20_24.popvalue as "pop20_24",
+        tbl25_29.popvalue as "pop25_29",
+        tbl30_34.popvalue as "pop30_34",
+        tbl35_39.popvalue as "pop35_39",
+        tbl40_44.popvalue as "pop40_44",
+        tbl45_49.popvalue as "pop45_49",
+        tbl50_54.popvalue as "pop50_54",
+        tbl55_59.popvalue as "pop55_59",
+        tbl60_64.popvalue as "pop60_64",
+        tbl65_69.popvalue as "pop65_69",
+        tbl70_74.popvalue as "pop70_74",
+        tbl75_100.popvalue as "pop75_100",
+        (COALESCE(tbl1_4.popvalue, 0.0)   + COALESCE(tbl5_9.popvalue, 0.0) +
+        COALESCE(tbl10_14.popvalue, 0.0) + COALESCE(tbl15_19.popvalue, 0.0) +
+        COALESCE(tbl20_24.popvalue, 0.0) + COALESCE(tbl25_29.popvalue, 0.0) +
+        COALESCE(tbl30_34.popvalue, 0.0) + COALESCE(tbl35_39.popvalue, 0.0) +
+        COALESCE(tbl40_44.popvalue, 0.0) + COALESCE(tbl45_49.popvalue, 0.0) +
+        COALESCE(tbl50_54.popvalue, 0.0) + COALESCE(tbl55_59.popvalue, 0.0) +
+        COALESCE(tbl60_64.popvalue, 0.0) + COALESCE(tbl65_69.popvalue, 0.0) +
+        COALESCE(tbl70_74.popvalue, 0.0) + COALESCE(tbl75_100.popvalue, 0.0)
+        ) as "pop_total"
+    FROM fn_GetWardPopSummary(0, 4, sourceTarget) as tbl1_4
+    LEFT JOIN fn_GetWardPopSummary(5, 9, sourceTarget) as tbl5_9
+      ON tbl1_4.globalid = tbl5_9.globalid
 
-  LEFT JOIN fn_GetWardPopSummary(10, 14) as tbl10_14
-    ON tbl5_9.globalid = tbl10_14.globalid
-  LEFT JOIN fn_GetWardPopSummary(15, 19) as tbl15_19
-    ON tbl10_14.globalid = tbl15_19.globalid
+    LEFT JOIN fn_GetWardPopSummary(10, 14, sourceTarget) as tbl10_14
+      ON tbl5_9.globalid = tbl10_14.globalid
+    LEFT JOIN fn_GetWardPopSummary(15, 19, sourceTarget) as tbl15_19
+      ON tbl10_14.globalid = tbl15_19.globalid
 
-  LEFT JOIN fn_GetWardPopSummary(20, 24) as tbl20_24
-    ON tbl15_19.globalid = tbl20_24.globalid
-  LEFT JOIN fn_GetWardPopSummary(25, 29) as tbl25_29
-    ON tbl20_24.globalid = tbl25_29.globalid
+    LEFT JOIN fn_GetWardPopSummary(20, 24, sourceTarget) as tbl20_24
+      ON tbl15_19.globalid = tbl20_24.globalid
+    LEFT JOIN fn_GetWardPopSummary(25, 29, sourceTarget) as tbl25_29
+      ON tbl20_24.globalid = tbl25_29.globalid
 
-  LEFT JOIN fn_GetWardPopSummary(30, 34) as tbl30_34
-    ON tbl25_29.globalid = tbl30_34.globalid
-  LEFT JOIN fn_GetWardPopSummary(35, 39) as tbl35_39
-    ON tbl30_34.globalid = tbl35_39.globalid
+    LEFT JOIN fn_GetWardPopSummary(30, 34, sourceTarget) as tbl30_34
+      ON tbl25_29.globalid = tbl30_34.globalid
+    LEFT JOIN fn_GetWardPopSummary(35, 39, sourceTarget) as tbl35_39
+      ON tbl30_34.globalid = tbl35_39.globalid
 
-  LEFT JOIN fn_GetWardPopSummary(40, 44) as tbl40_44
-    ON tbl35_39.globalid = tbl40_44.globalid
-  LEFT JOIN fn_GetWardPopSummary(45, 49) as tbl45_49
-    ON tbl40_44.globalid = tbl45_49.globalid
+    LEFT JOIN fn_GetWardPopSummary(40, 44, sourceTarget) as tbl40_44
+      ON tbl35_39.globalid = tbl40_44.globalid
+    LEFT JOIN fn_GetWardPopSummary(45, 49, sourceTarget) as tbl45_49
+      ON tbl40_44.globalid = tbl45_49.globalid
 
-  LEFT JOIN fn_GetWardPopSummary(50, 54) as tbl50_54
-    ON tbl45_49.globalid = tbl50_54.globalid
-  LEFT JOIN fn_GetWardPopSummary(55, 59) as tbl55_59
-    ON tbl50_54.globalid = tbl55_59.globalid
+    LEFT JOIN fn_GetWardPopSummary(50, 54, sourceTarget) as tbl50_54
+      ON tbl45_49.globalid = tbl50_54.globalid
+    LEFT JOIN fn_GetWardPopSummary(55, 59, sourceTarget) as tbl55_59
+      ON tbl50_54.globalid = tbl55_59.globalid
 
-  LEFT JOIN fn_GetWardPopSummary(60, 64) as tbl60_64
-    ON tbl55_59.globalid = tbl60_64.globalid
-  LEFT JOIN fn_GetWardPopSummary(65, 69) as tbl65_69
-    ON tbl60_64.globalid = tbl65_69.globalid
+    LEFT JOIN fn_GetWardPopSummary(60, 64, sourceTarget) as tbl60_64
+      ON tbl55_59.globalid = tbl60_64.globalid
+    LEFT JOIN fn_GetWardPopSummary(65, 69, sourceTarget) as tbl65_69
+      ON tbl60_64.globalid = tbl65_69.globalid
 
-  LEFT JOIN fn_GetWardPopSummary(70, 74) as tbl70_74
-    ON tbl65_69.globalid = tbl70_74.globalid
-  LEFT JOIN fn_GetWardPopSummary(75, 100) as tbl75_100
-    ON tbl70_74.globalid = tbl75_100.globalid;
+    LEFT JOIN fn_GetWardPopSummary(70, 74, sourceTarget) as tbl70_74
+      ON tbl65_69.globalid = tbl70_74.globalid
+    LEFT JOIN fn_GetWardPopSummary(75, 100, sourceTarget) as tbl75_100
+      ON tbl70_74.globalid = tbl75_100.globalid;
+  END $FF1$
+  LANGUAGE 'plpgsql';
 
   -- FUNCTION: fn_BuildPopTables
   CREATE OR REPLACE FUNCTION fn_BuildPopTablesPerZone()
